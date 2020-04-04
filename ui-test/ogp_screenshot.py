abb023c029c560ba80c5ae6fc7998a92a53e04eb
ynakahira <ynakahira@MBP2017.local>
2020-04-04 22:33:07 +0900

    hotfix: ogp images

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -2,6 +2,9 @@ import os
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.common import exceptions
 
 if not os.path.exists("ogp"):
     os.mkdir("ogp")
@@ -13,22 +16,28 @@ PATHS = {
     "/cards/attributes-of-confirmed-cases": (959, 480),
     "/cards/number-of-tested": (959, 540),
     "/cards/number-of-reports-to-covid19-telephone-advisory-center": (959, 500),
-    "/cards/number-of-reports-to-covid19-consultation-desk": (959, 500),
-    "/cards/predicted-number-of-toei-subway-passengers": (959, 750),
+    #"/cards/number-of-reports-to-covid19-consultation-desk": (959, 500),
+    #"/cards/predicted-number-of-toei-subway-passengers": (959, 750),
     "/cards/agency": (959, 730),
     "/cards/details-of-tested-cases": (959, 500),
     "/cards/number-of-inspection-persons": (959, 600),
-    "/cards/shinjuku-visitors": (959, 820),
-    "/cards/chiyoda-visitors": (959, 820),
-    "/cards/shinjuku-st-heatmap": (959, 600),
-    "/cards/tokyo-st-heatmap": (959, 600)
+    #"/cards/shinjuku-visitors": (959, 820),
+    #"/cards/chiyoda-visitors": (959, 820),
+    #"/cards/shinjuku-st-heatmap": (959, 600),
+    #"/cards/tokyo-st-heatmap": (959, 600)
 }
+options = Options()
+#prefs = {"profile.managed_default_content_settings.images": 2} # no images
+#options.add_experimental_option("prefs", prefs)
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--hide-scrollbars")
+# CUI
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+#options.add_argument('--no-sandbox')
+#options.add_argument('--window-size=1200x600')
 
-driver = webdriver.Chrome(options=options)
+
+driver = webdriver.Chrome(options=options, executable_path='./chromedriver')
 
 for lang in ("ja", "en", "zh-cn", "zh-tw", "ko", "ja-basic"):
     if not os.path.exists("ogp/{}".format(lang)):
@@ -36,7 +45,7 @@ for lang in ("ja", "en", "zh-cn", "zh-tw", "ko", "ja-basic"):
     for path, size in PATHS.items():
         driver.set_window_size(*size)
         driver.get(
-            "http://localhost:8000{}?ogp=true".format(
+            "http://localhost:3000{}?ogp=true".format(
                 path if lang == "ja" else "/{}{}".format(lang, path)
             )
         )
